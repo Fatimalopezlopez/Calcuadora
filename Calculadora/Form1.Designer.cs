a6fc833b9cd378d9cb40bbb82c12f9cce329f7a3
FATIMA\FatimaBerenice <Fatima.style@hotmail.com>
2018-02-09 21:35:36 -0600

    CALCULADORA  SENCILLA

---
+++ Calculadora/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtexpresion = new System.Windows.Forms.TextBox();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtresultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtexpresion
+            // 
+            this.txtexpresion.Location = new System.Drawing.Point(12, 98);
+            this.txtexpresion.Name = "txtexpresion";
+            this.txtexpresion.Size = new System.Drawing.Size(235, 20);
+            this.txtexpresion.TabIndex = 0;
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(172, 169);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(75, 23);
+            this.btncalcular.TabIndex = 1;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Expresión";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Resultado";
+            // 
+            // txtresultado
+            // 
+            this.txtresultado.Location = new System.Drawing.Point(12, 229);
+            this.txtresultado.Name = "txtresultado";
+            this.txtresultado.Size = new System.Drawing.Size(235, 20);
+            this.txtresultado.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtresultado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.txtexpresion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtexpresion;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtresultado;
+    }
+}
+
---
+++ Calculadora/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using com.calitha.goldparser;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        MyParser parser;
+        public Form1()
+        {
+            parser = new MyParser(Application.StartupPath + "\\Calculadora.cgt");
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            parser.Parse(txtexpresion.Text);
+            txtresultado.Text = parser.resultado;
+            if(parser.resultado== "∞")
+            {
+                txtresultado.Text = "Math ERROR";
+            }
+        }
+    }
+}
